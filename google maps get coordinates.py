a62c22d0b7003c5ae762baa063cc1b1edc2ae3bc
Tishan Ravisanka <ktrwickramasinghe@gmail.com>
2021-01-22 23:57:37 +0530

    Add files via upload

---
+++ google maps get coordinates.py
@@ -0,0 +1,36 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+# Replace your Google Places API key, enter it here (need to enable billing)
+api_key = 'AIzaSy___IDByT70'
+serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+
+    data = uh.read()
+    print('Retrieved', len(data), 'characters')
+    print(data.decode())
+    tree = ET.fromstring(data)
+
+    results = tree.findall('result')
+    lat = results[0].find('geometry').find('location').find('lat').text
+    lng = results[0].find('geometry').find('location').find('lng').text
+    location = results[0].find('formatted_address').text
+
+    print('lat', lat, 'lng', lng)
+    print(location)
